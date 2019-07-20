09469132c1199bb4e2be1fe68a79804ac055fded
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-07-20 10:14:29 +0900

    Fix

--- A star.py
+++ A star.py
@@ -1,8 +1,7 @@
 class Node():
-
     def __init__(self, parent=None, position=None):
-        self.parent = parent # 부모 노드
-        self.position = position # 노드 위치
+        self.parent = parent  # 부모 노드
+        self.position = position  # 노드 위치
 
         # 노드의 g,h,f
         self.g = 0
@@ -13,60 +12,61 @@ class Node():
         return self.position == other.position
 
 def astar(maze, start, end):
-
-    start_node = Node(None, start) # 시작 노드 생성
+    start_node = Node(None, start)  # 시작 노드 생성
     start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end) # 도착 노드 생성
+    end_node = Node(None, end)  # 도착 노드 생성
     end_node.g = end_node.h = end_node.f = 0
 
-    open_list = [] # 오픈 리스트
-    closed_list = [] # 닫힌 리스트
+    open_list = []  # 오픈 리스트
+    closed_list = []  # 닫힌 리스트
 
-    open_list.append(start_node) # 오픈 리스트 처음은 시작 노드 값으로
+    open_list.append(start_node)  # 오픈 리스트 처음은 시작 노드 값으로
 
-    while len(open_list) > 0: # 오픈 리스트에 노드가 없을때 종료
+    while len(open_list) > 0:  # 오픈 리스트에 노드가 없을때 종료
         current_node = open_list[0]
         current_index = 0
-        for index, item in enumerate(open_list): # 오픈 리스트 노드들중에 가장 최소 비용 F를 가진 노드를 찾는다
+        for index, item in enumerate(open_list):  # 오픈 리스트 노드들중에 가장 최소 비용 F를 가진 노드를 찾는다
             if item.f < current_node.f:
                 current_node = item
                 current_index = index
 
-        open_list.pop(current_index) # 오픈 리스트에 해당하는 노드 pop
-        closed_list.append(current_node) # 닫힌 리스트에 해당 노드 input
+        open_list.pop(current_index)  # 오픈 리스트에 해당하는 노드 pop
+        closed_list.append(current_node)  # 닫힌 리스트에 해당 노드 input
 
-        if current_node == end_node: # 도착 노드까지 탐색이 완료될 경우
+        if current_node == end_node:  # 도착 노드까지 탐색이 완료될 경우
             path = []
             current = current_node
             while current is not None:
                 path.append(current.position)
                 current = current.parent
-            return path[::-1] # 역순으로 출력되게 반전 상태로 리턴 시킨다
+            return path[::-1]  # 역순으로 출력되게 반전 상태로 리턴 시킨다
 
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # 동서남북 그리고 대각선 방향으로 한칸씩 이동
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1),
+                             (1, 1)]:  # 동서남북 그리고 대각선 방향으로 한칸씩 이동
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             if node_position[0] > (len(maze) - 1) or node_position[0] < 0 \
-                    or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0: # 이동될 지점이 maze 바깥일때
+                    or node_position[1] > (len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:  # 이동될 지점이 maze 바깥일때
                 continue
 
-            if maze[node_position[0]][node_position[1]] != 0: # 이동될 지점이 벽일때
+            if maze[node_position[0]][node_position[1]] != 0:  # 이동될 지점이 벽일때
                 continue
 
             new_node = Node(current_node, node_position)
 
-            children.append(new_node) # 자식 노드에 이동할 지점 즉 이동 노드 값 대입
+            children.append(new_node)  # 자식 노드에 이동할 지점 즉 이동 노드 값 대입
 
         for child in children:
             check = False
             for closed_child in closed_list:
-                if child == closed_child: # 탐색중인 자식 노드가 닫힌 리스트와 같은 값일 경우
+                if child == closed_child:  # 탐색중인 자식 노드가 닫힌 리스트와 같은 값일 경우
                     check = True
 
             if check == False:
                 child.g = current_node.g + 1
-                child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2) # 수평방향은 10, 대각선은 12
+                child.h = ((child.position[0] - end_node.position[0]) ** 2) + (
+                            (child.position[1] - end_node.position[1]) ** 2)  # 수평방향은 10, 대각선은 12
                 child.f = child.g + child.h
 
                 for open_node in open_list:
@@ -75,9 +75,8 @@ def astar(maze, start, end):
 
                 open_list.append(child)
 
-
 def main():
-    #미로 생성
+    # 미로 생성
     maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -89,17 +88,16 @@ def main():
             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    start = (0, 0) # 시작 좌표
-    end = (0,5) #도착 좌표
+    start = (0, 0)  # 시작 좌표
+    end = (0, 5)  # 도착 좌표
 
-    if maze[end[0]][end[1]] == 1: # 도착지가 벽일 경우
+    if maze[end[0]][end[1]] == 1:  # 도착지가 벽일 경우
         print("도착지는 벽으로 구성되어 있습니다")
         return
 
-    path = astar(maze, start, end) #a star 탐색 결과
-    print("도착지까지의 최소 경로는 : ",path) # 경로 출력
-    print("경로에 드는 비용은 : ",len(path)) # 최소 비용 출력
-
+    path = astar(maze, start, end)  # a star 탐색 결과
+    print("도착지까지의 최소 경로는 : ", path)  # 경로 출력
+    print("경로에 드는 비용은 : ", len(path))  # 최소 비용 출력
 
 if __name__ == '__main__':
     main()
--- A-star_Maze(sample).py
+++ A-star_Maze(sample).py
@@ -1,23 +1,22 @@
 import pygame
 import sys
 from time import sleep
-import tkinter
 
-white = (255,255,255)
-gold = (255,204,0)
-green = (0,255,0)
-dark_green=(0,153,0)
-black=(0,0,0)
-size = [800, 600]
-box_size = 45 # setup box size
-player_size = 20 # setup player size
+white = (255, 255, 255)
+gold = (255, 204, 0)
+green = (0, 255, 0)
+dark_green = (0, 153, 0)
+black = (0, 0, 0)
+
+size = [800, 600]  # setup game size
+box_size = 45  # setup box size
+player_size = 20  # setup player size
 
 start_point = (0, 1)  # start x,y
 end_point = (15, 9)  # end x,y
 
 maze_x = 17  # max maze x
 maze_y = 11  # max maze y
-
 
 make_maze = [[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
@@ -35,70 +34,64 @@ class MAZE:
     def __init__(self):
         pygame.init()
 
-        self.display = pygame.display.set_mode(size) # display setup
-        pygame.display.set_caption('maze') # caption setup
-        pygame.display.set_icon(pygame.image.load('img/maze.png')) # icon setup
-        self.fps = pygame.time.Clock() # fps setup
+        self.display = pygame.display.set_mode(size)  # display setup
+        pygame.display.set_caption('maze')  # caption setup
+        pygame.display.set_icon(pygame.image.load('img/maze.png'))  # icon setup
+        self.fps = pygame.time.Clock()  # fps setup
 
-        self.player_x = 70 # pixcel x
-        self.player_y = 20 # pixecl y
+        self.player_x = 70  # pixcel x
+        self.player_y = 20  # pixecl y
 
-        self.x = start_point[1] # real x
-        self.y = start_point[0] # real y
+        self.x = start_point[1]  # real x
+        self.y = start_point[0]  # real y
 
-        self.font = pygame.font.SysFont("Arial", 40) # font setup
-        self.cnt=0
-        self.endcnt=0
+        self.font = pygame.font.SysFont("Arial", 40)  # font setup
+        self.cnt = 0
 
         self.maze()
 
-    def maze(self):
-        #draw maze
+    def maze(self):# draw maze
         mx = 0
         my = 0
 
-        for i in range(0,maze_x*maze_y):
+        for i in range(0, maze_x * maze_y):
             if make_maze[my][mx] == 3:
-                pygame.draw.rect(self.display, gold, (mx*box_size, my*box_size, box_size, box_size))
-            if make_maze[my][mx]==2:
+                pygame.draw.rect(self.display, gold, (mx * box_size, my * box_size, box_size, box_size))
+            if make_maze[my][mx] == 2:
                 pygame.draw.rect(self.display, green, (mx * box_size, my * box_size, box_size, box_size))
-            if make_maze[my][mx] ==1:
-                pygame.draw.rect(self.display, white, (mx*box_size, my*box_size, box_size, box_size),2)
-            mx +=1
+            if make_maze[my][mx] == 1:
+                pygame.draw.rect(self.display, white, (mx * box_size, my * box_size, box_size, box_size), 2)
+            mx += 1
             if mx == maze_x:
-                mx=0
-                my+=1
+                mx = 0
+                my += 1
 
+    def char(self): # setup character
+        self.player = pygame.Rect(self.player_x, self.player_y, player_size, player_size)  # character setup
 
-    def char(self):
-        self.player = pygame.Rect(self.player_x,self.player_y,player_size,player_size) # character setup
-
-    def move(self):
-        key=pygame.key.get_pressed()
+    def move(self): # check the key press and move
+        key = pygame.key.get_pressed()
         speed = 0.3
 
-        if self.y==end_point[1] and self.x==end_point[0]:
-            self.endcnt = self.cnt
-            print("도착지까지비용 : " +str(self.endcnt))
+        if self.y == end_point[1] and self.x == end_point[0]:
+            print("도착!")
+            print("도착지까지의 비용은 : " + str(self.cnt))
             sys.exit()
 
-        if (self.y==start_point[0] and self.x==start_point[1]) and self.cnt >0:
-            self.cnt=0
-            mx=0
-            my=0
+        if (self.y == start_point[0] and self.x == start_point[1]) and self.cnt > 0: # draw last move point
+            self.cnt = 0
+            mx = 0
+            my = 0
             for i in range(0, maze_x * maze_y):
                 if make_maze[my][mx] == 2:
-                    make_maze[my][mx]=0
-                    pygame.draw.rect(self.display, white, (mx * box_size, my * box_size, box_size, box_size),2)
-                mx+=1
+                    make_maze[my][mx] = 0
+                    pygame.draw.rect(self.display, white, (mx * box_size, my * box_size, box_size, box_size), 2)
+                mx += 1
                 if mx == maze_x:
                     mx = 0
                     my += 1
 
-
-
-
-        if key[pygame.K_RIGHT]: # move right
+        if key[pygame.K_RIGHT]:  # move right
             if make_maze[self.y][self.x + 1] == 1:
                 pass
             elif make_maze[self.y][self.x + 1] == 2:
@@ -111,13 +104,13 @@ class MAZE:
                 sleep(speed)
             else:
                 make_maze[self.y][self.x] = 2
-                pygame.draw.circle(self.display,white,(self.player_x,self.player_y),player_size)
+                pygame.draw.circle(self.display, white, (self.player_x, self.player_y), player_size)
                 self.player_x += box_size
-                self.x+=1
-                self.cnt+=1
+                self.x += 1
+                self.cnt += 1
                 sleep(speed)
 
-        elif key[pygame.K_LEFT]: # move left
+        elif key[pygame.K_LEFT]:  # move left
             if make_maze[self.y][self.x - 1] == 1:
                 pass
             elif make_maze[self.y][self.x - 1] == 2:
@@ -132,14 +125,14 @@ class MAZE:
                 make_maze[self.y][self.x] = 2
                 pygame.draw.circle(self.display, white, (self.player_x, self.player_y), player_size)
                 self.player_x -= box_size
-                self.x-=1
+                self.x -= 1
                 self.cnt += 1
                 sleep(speed)
 
-        elif key[pygame.K_UP]: # move up
-            if make_maze[self.y-1][self.x] == 1:
+        elif key[pygame.K_UP]:  # move up
+            if make_maze[self.y - 1][self.x] == 1:
                 pass
-            elif make_maze[self.y-1][self.x] == 2:
+            elif make_maze[self.y - 1][self.x] == 2:
                 make_maze[self.y][self.x] = 0
                 self.cnt -= 1
                 pygame.draw.circle(self.display, green, (self.player_x, self.player_y), player_size)
@@ -152,14 +145,14 @@ class MAZE:
                 make_maze[self.y][self.x] = 2
                 pygame.draw.circle(self.display, white, (self.player_x, self.player_y), player_size)
                 self.player_y -= box_size
-                self.y-=1
+                self.y -= 1
                 self.cnt += 1
                 sleep(speed)
 
-        elif key[pygame.K_DOWN]: # move down
-            if make_maze[self.y+1][self.x] == 1:
+        elif key[pygame.K_DOWN]:  # move down
+            if make_maze[self.y + 1][self.x] == 1:
                 pass
-            elif make_maze[self.y+1][self.x] == 2:
+            elif make_maze[self.y + 1][self.x] == 2:
                 make_maze[self.y][self.x] = 0
                 self.cnt -= 1
                 pygame.draw.circle(self.display, green, (self.player_x, self.player_y), player_size)
@@ -172,18 +165,13 @@ class MAZE:
                 make_maze[self.y][self.x] = 2
                 pygame.draw.circle(self.display, white, (self.player_x, self.player_y), player_size)
                 self.player_y += box_size
-                self.y+=1
+                self.y += 1
                 self.cnt += 1
                 sleep(speed)
 
-
-
-    def show(self):
-
-        showcount = self.font.render("score:" + str(self.cnt), False, gold)
-        self.display.blit(showcount, (600, 500))
-
-
+    def show(self): # display Move count string
+        showcount = self.font.render("Move:" + str(self.cnt), False, gold)
+        self.display.blit(showcount, (650, 500))
 
     def run(self):
         while True:
@@ -200,10 +188,11 @@ class MAZE:
             self.char()
             self.show()
 
-            pygame.draw.circle(self.display,green,(self.player.left,self.player.top),player_size) # draw character
-            pygame.draw.circle(self.display, dark_green, (self.player.left-1, self.player.top-1), player_size)  # draw gradation
+            pygame.draw.circle(self.display, green, (self.player.left, self.player.top), player_size)  # draw character
+            pygame.draw.circle(self.display, dark_green, (self.player.left - 1, self.player.top - 1),player_size)  # draw gradation
 
             pygame.display.flip()
 
-if __name__=="__main__": # start main
+
+if __name__ == "__main__":  # start main
     MAZE().run()
